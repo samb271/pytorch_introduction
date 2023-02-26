06c9ea089b48b6db955a155f21a080a8d96ef836
samb271 <footballsam1997@hotmail.com>
2023-02-26 15:16:12 -0500

    init

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import torch
+from torch import nn
+import matplotlib.pyplot as plt
 
-print(torch.__version__)
+# DATA: PREPARING AND LOADING
+
+
+# Machine learning is a game of 2 parts:
+#  1. Get data into a numerical representation
+#  2. Build a model to learn patterns in that numerical representation
+
